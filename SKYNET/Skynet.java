ea788476bcec6447fb87a3509209c339c24eac4e
Feargal Kavanagh <feargkav@gmail.com>
2016-02-11 23:00:03 +0000

    Removed the Eclipse Folder: Not really needed

---
+++ SKYNET/Skynet.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Skynet {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Welcome to Risk!");
+		Game game = new Game();
+		game.GameStart();
+
+	}
+
+}
