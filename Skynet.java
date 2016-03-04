de37d65b2bc8d6e9748c7e86b2255aaf95a528a0
Feargal Kavanagh <feargkav@gmail.com>
2016-03-04 17:30:26 +0000

    neatened

---
+++ Skynet.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Skynet {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Welcome to Risk!");
+		Game Game = new Game();
+		Game.main(args);
+
+	}
+
+}
